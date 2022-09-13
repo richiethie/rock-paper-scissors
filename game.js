a34b71d9d56ca86a2863cda1b004890234a99837
Richie Thiesfeldt <richiethie@gmail.com>
2022-09-12 19:37:10 -0500

    Added pseudo-code

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-console.log("yo")
+// define array for choices [r,p,s ]
+// define var for computerChoice
+// define var for playerChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+// prompt() playerChoice
+
+// randomly choose computerChoice
+
+// compare choices
+    // if playerChoice is p and computerChoice is r
+        //
+
+// display (alert) comparison results (won, tied, lost round)
+
+// show stats (number of wins, losses, ties)
+
+// ask to play again?
+    // restart game
+// else
+    // end game
