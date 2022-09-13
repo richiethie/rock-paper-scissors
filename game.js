515df9e8ce5858f5386d8cda6a16c373afb428f6
Richie Thiesfeldt <richiethie@gmail.com>
2022-09-12 21:43:20 -0500

    Completed Rock, Paper, Scissors

--- game.js
+++ game.js
@@ -1,44 +1,56 @@
-// define array for choices [r,p,s ]
-var choice = ['R', 'P', 'S']
-// define var for computerChoice
-var robot = Math.floor(Math.random() * choice.length);
-// define var for humanChoice
-// var humanChoice = 
-// define var for wins
-// define var for losses
-// define var for ties
 var w = 0;
 var l = 0;
 var t = 0;
 
-// prompt() humanChoice
-var person = prompt('Enter R, P, or S:');
-person = person.toUpperCase();
-// randomly choose computerChoice
-var robotChoice = alert("Computer chose:" + robot);
+var choice = ['R', 'P', 'S']
 
-// compare choices
-    // if humanChoice is p and computerChoice is r
-        //
-if (person === robotChoice) {
-    alert("It's a tie!");
-    t++;
-} else if (
-    (person === 'R' && robotChoice === 'S')
-    (person === 'P' && robotChoice === 'R')
-    (person === 'S' && robotChoice === 'P')
-    ) {
-    alert("You win!");
-    w++;
-} else {
-    alert("You lost.");
-    l++;
+var game = function() {
+    // define array for choices [r,p,s ]
+
+    // define var for computerChoice
+    var robot = Math.floor(Math.random() * choice.length);
+    var robotChoice = choice[robot];
+    // define var for humanChoice
+    // var humanChoice = 
+    // define var for wins
+    // define var for losses
+    // define var for ties
+
+
+    // prompt() humanChoice
+    var person = prompt('Enter R, P, or S:');
+    person = person.toUpperCase();
+    // randomly choose computerChoice
+    alert("Computer chose: " + robotChoice);
+
+    // compare choices
+        // if humanChoice is p and computerChoice is r
+            //
+    if (person === robotChoice) {
+        alert("It's a tie!");
+        t++;
+    } else if (
+        person === 'R' && robotChoice === 'S' ||
+        person === 'P' && robotChoice === 'R' ||
+        person === 'S' && robotChoice === 'P'
+        ) {
+        alert("You win!");
+        w++;
+    } else {
+        alert("You lost.");
+        l++;
+    }
+    // display (alert) comparison results (won, tied, lost round)
+
+    // show stats (number of wins, losses, ties)
+    alert("Stats: \n Wins: " + w + "\n Losses: " + l + "\n Ties: " + t);
+    // ask to play again?
+    var playAgain = confirm("Play Again?");
+    if (playAgain) {
+        game();
+    }
+        // restart game
+    // else
+        // end game
 }
-// display (alert) comparison results (won, tied, lost round)
-
-// show stats (number of wins, losses, ties)
-
-// ask to play again?
-    // restart game
-// else
-    // end game
+game()
