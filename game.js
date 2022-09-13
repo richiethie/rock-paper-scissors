481bcf37893aba175c57716347d585df1c917b84
Richie Thiesfeldt <richiethie@gmail.com>
2022-09-12 20:08:59 -0500

    added if else statements

--- game.js
+++ game.js
@@ -1,18 +1,39 @@
 // define array for choices [r,p,s ]
+var choice = ['R', 'P', 'S']
 // define var for computerChoice
-// define var for playerChoice
+var robot = Math.floor(Math.random() * choice.length);
+// define var for humanChoice
+// var humanChoice = 
 // define var for wins
 // define var for losses
 // define var for ties
+var w = 0;
+var l = 0;
+var t = 0;
 
-// prompt() playerChoice
-
+// prompt() humanChoice
+var person = prompt('Enter R, P, or S:');
+person = person.toUpperCase();
 // randomly choose computerChoice
+var robotChoice = alert("Computer chose:" + robot);
 
 // compare choices
-    // if playerChoice is p and computerChoice is r
+    // if humanChoice is p and computerChoice is r
         //
-
+if (person === robotChoice) {
+    alert("It's a tie!");
+    t++;
+} else if (
+    (person === 'R' && robotChoice === 'S')
+    (person === 'P' && robotChoice === 'R')
+    (person === 'S' && robotChoice === 'P')
+    ) {
+    alert("You win!");
+    w++;
+} else {
+    alert("You lost.");
+    l++;
+}
 // display (alert) comparison results (won, tied, lost round)
 
 // show stats (number of wins, losses, ties)
